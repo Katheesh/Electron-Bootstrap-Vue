4a771204056c6f3b25c37ef708337d3e2ad5af66
katheesh <katheeskumar@outlook.com>
2020-09-29 14:32:11 +0530

    removed some comments

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,8 @@ const routes = [
   {
     path: "/register",
     name: "Register",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Register.vue")
+      import("../views/Register.vue")
   },
   {
     path: "/login",
